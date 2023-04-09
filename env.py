4b8d56cdefd88fb1739c05b08123291f6b644e76
Peter <peter@einstein>
2023-04-08 22:44:44 -0600

    logging data to wandb

--- env.py
+++ env.py
@@ -1,9 +1,13 @@
 import argparse
+import pickle
+import time
+from pathlib import Path
 from typing import Optional
 
 import mujoco
 import numpy as np
 import rerun as rr
+import wandb
 from mujoco._structs import _MjModelGeomViews, _MjDataGeomViews
 from scipy.spatial.transform import Rotation
 from trimesh.creation import box, cylinder
@@ -80,10 +84,9 @@ def rr_init():
 
 class Env:
 
-    def __init__(self, env_type):
-        self.model = mujoco.MjModel.from_xml_path('source.xml')
+    def __init__(self, model_xml_filename):
+        self.model = mujoco.MjModel.from_xml_path(model_xml_filename)
         self.data = mujoco.MjData(self.model)
-        self.env_type = env_type
 
     def step(self, action: Optional[np.ndarray]):
         if action is not None:
@@ -128,41 +131,69 @@ def main():
     np.set_printoptions(precision=3, suppress=True, linewidth=250)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('env_type', type=str, choices=['source', 'target'])
+    parser.add_argument('model_xml_filename', type=str)
     parser.add_argument('--seed', type=int, default=0)
-    parser.add_argument('--n-transitions', type=int, default=20)
+    parser.add_argument('--n-transitions', type=int, default=1000)
 
     args = parser.parse_args()
 
     rr_init()
 
-    env = Env(args.env_type)
-
-    env.step(None)
-
+    env_name = Path(args.model_xml_filename).stem
     dataset = []
-    before = env.get_state()
 
-    for transition_idx in range(args.n_transitions):
-        action = np.array([0.05, 0])
+    rng = np.random.RandomState(args.seed)
 
-        env.step(action)
-        after = env.get_state()
+    # run a bunch of smaller push episodes
+    while len(dataset) < args.n_transitions:
+        episode_t0 = time.time()
 
-        transition = {
-            'before': before,
-            'action': action,
-            'after': after,
-        }
+        # re-initialize the simulation
+        env = Env(args.model_xml_filename)
+        env.step(None)
+        before = env.get_state()
 
-        rr.log_scalar('state/obj_x', before['object_pos'][0], label='obj  x')
-        rr.log_scalar('state/obj_y', before['object_pos'][1], label='obj  y')
-        rr.log_scalar('state/robot_x', before['robot_pos'][0], label='robot  x')
-        rr.log_scalar('state/robot_y', before['robot_pos'][1], label='robot  y')
+        # sample a nominal velocity
+        nominal_x_vel = rng.uniform(0.02, 0.1)
+        nominal_y_vel = rng.normal(0, 0.02)
 
-        dataset.append(transition)
+        for _ in range(10):
+            action = np.array([nominal_x_vel, nominal_y_vel]) + rng.normal(0, 0.005, size=(2,))
 
-        before = after
+            env.step(action)
+            after = env.get_state()
+
+            transition = {
+                'before': before,
+                'action': action,
+                'after': after,
+            }
+
+            rr.log_scalar('state/obj_x', before['object_pos'][0], label='obj x')
+            rr.log_scalar('state/obj_y', before['object_pos'][1], label='obj y')
+            rr.log_scalar('state/robot_x', before['robot_pos'][0], label='robot x')
+            rr.log_scalar('state/robot_y', before['robot_pos'][1], label='robot y')
+
+            dataset.append(transition)
+
+            before = after
+
+        print(f'episode took {time.time() - episode_t0:.2f}s')
+
+    with open('dataset.pkl', 'wb') as f:
+        pickle.dump(dataset, f)
+
+    run = wandb.init(project='pushing_focus', entity='armlab', config={
+        'env_name': env_name,
+        'seed': args.seed,
+        'n_transitions': args.n_transitions,
+    })
+
+    artifact = wandb.Artifact(f'{env_name}-dataset', type='dataset')
+    artifact.add_file('dataset.pkl')
+    run.log_artifact(artifact)
+
+    wandb.finish()
 
 
 if __name__ == '__main__':
